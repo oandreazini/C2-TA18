89afce420bde538307bafba7a3e64408a498b651
pbesora <pau_besora@hotmail.com>
2022-07-28 10:23:56 +0200

    Actualizado createTable

--- RTS-C2-UD18/src/metodosConexion/ConexionDB.java
+++ RTS-C2-UD18/src/metodosConexion/ConexionDB.java
@@ -15,7 +15,7 @@ public class ConexionDB {
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			c = DriverManager.getConnection("jdbc:mysql://localhost:3309","USER","PASSWORD");
+			c = DriverManager.getConnection("jdbc:mysql://192.168.1.55:3309","root","PASSWORD");
 			
 			System.out.println("Server Connected");
 			
@@ -26,7 +26,16 @@ public class ConexionDB {
 		}
 	}
 
-	
+	public void MySQLConnection(String user, String pass, String db) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			c = DriverManager.getConnection("jdbc:mysql://192.168.1.55:3309/" + db , user, pass);
+			
+		} catch (SQLException | ClassNotFoundException e) {
+			System.out.println("No se ha podido conectar con mi base de datos");
+			System.out.println(e);
+		}
+	}
 	
 	public void closeConnection() {
 		
@@ -48,12 +57,29 @@ public class ConexionDB {
 			Statement st = c.createStatement();
 			st.executeUpdate(Query);
 			closeConnection();
-			//MySQLConnection("root", "", name);
+			MySQLConnection("root", "", name);
 			System.out.println("Se ha creado la base de datos " + name + " de forma exitosa.");
 		} catch (SQLException ex) {
 			Logger.getLogger(ConexionDB.class.getName()).log(Level.SEVERE, null, ex);
 		}
 	}
 	
+	
+	public void createTable(String db, String name, String campos) {
+		try{
+			String Querydb = "USE " + db + ";";
+			Statement stdb = c.createStatement();
+			stdb.executeUpdate(Querydb);
+			
+			String Query = "CREATE TABLE " + name + "(" + campos + ");";
+			Statement st = c.createStatement();
+			st.executeUpdate(Query);
+			System.out.println("Tabla creada con exito!");
+			
+		}catch(SQLException e){
+			System.out.println(e.getMessage());
+			System.out.println("Error creando tabla.");
+		}
+	}
 	
 }
