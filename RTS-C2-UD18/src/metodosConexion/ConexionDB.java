c8d2691f0c6a2c65626ab8a602fa0bcb8bc31402
oandreazini <oriol_as_92@hotmail.com>
2022-07-28 15:23:13 +0200

    Correción

--- RTS-C2-UD18/src/metodosConexion/ConexionDB.java
+++ RTS-C2-UD18/src/metodosConexion/ConexionDB.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 
 public class ConexionDB {
 	
-	private String pass = "SONYvaio5.";
+	private String pass = ".";
 	private String user = "root";
 	private Connection c;
 	
