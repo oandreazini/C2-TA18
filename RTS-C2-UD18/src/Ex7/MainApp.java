9fbc83c09fa65b7215a9a5989e84497ccc1af66e
Oriol_PC <Oriol_PC@192.168.1.110>
2022-07-29 21:22:26 +0200

    Ex5

--- RTS-C2-UD18/src/Ex7/MainApp.java
+++ RTS-C2-UD18/src/Ex7/MainApp.java
@@ -1,19 +1,25 @@
 package Ex7;
 
+import metodosConexion.ConexionDB;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ConexionDB conexionDB = new ConexionDB("Ex7");
+		
+		Cientificos c = new Cientificos();
+		Proyecto p = new Proyecto();
+		AsignadoA a = new AsignadoA();
+		
+		createTables(conexionDB);
 		
 		createInserts();
 	}
 	
 	public static void createInserts () {
 
-		Cientificos c = new Cientificos();
-		Proyecto p = new Proyecto();
-		AsignadoA a = new AsignadoA();
+		
 
 		for(int i=0; i<5; i++) {
 			c.insert("Ex7");
